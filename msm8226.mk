8b70fbbc98450873b9f88af2d6ee56fdb83a5435
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-06-23 19:17:24 +0530

    msm8226-common: Remove health hal 2.0 manifest entry.
    Health hal is being up-rev'ed to 2.1, and it now
    gets the manifest entry from its vintf fragment.
    So, remove the earlier set 2.0 entry.
    
    Change-Id: I034ad8fa2a32e1221343c1eb23528c79d51e7b1b
    
    msm8226-common: Uprev Health HAL v2.1

--- msm8226.mk
+++ msm8226.mk
@@ -254,8 +254,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
