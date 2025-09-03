05463db176123c8f45cbb8d65643c4b892724a78
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-09-03 20:39:18 +0530

    msm8226-common: Build missing a2dp entries

--- msm8226.mk
+++ msm8226.mk
@@ -83,6 +83,12 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# A2DP
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.msm8226 \
