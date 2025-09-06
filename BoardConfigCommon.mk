LineageOS/android_device_samsung_msm8226-common

msm8226-common: Don't let builds complete without vendor tree
* There is zero reason to ever build without blobs. We've even seen
  this situation with official builds from our servers. It's always
  better for a build to fail than it is for it to produce something
  that has no chance at working.

Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2018,2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -134,4 +134,4 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # inherit from the proprietary version
--include vendor/samsung/msm8226-common/BoardConfigVendor.mk
+include vendor/samsung/msm8226-common/BoardConfigVendor.mk

Dheeraj3031A/android_device_motorola_msm8226-common

BoardConfigCommon.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # inherit from the proprietary version
--include vendor/motorola/msm8226-common/BoardConfigVendor.mk
+include vendor/motorola/msm8226-common/BoardConfigVendor.mk
 
 VENDOR_PATH := device/motorola/msm8226-common
 