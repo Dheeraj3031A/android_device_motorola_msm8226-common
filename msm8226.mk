LineageOS/android_device_samsung_msm8226-common

msm8226-common: Kill legacy keystore
Legacy credential hash support was obsolete and got removed completely.
From now on all the devices need to rely on gatekeeper, which
in case of legacy devices should be the software implementation.

This fixes setting a lockscreen PIN/password/pattern.

Ref: https://android.googlesource.com/platform/frameworks/base/+/a758727c963948ae07e97c977a63b882216e8971

Change-Id: I020d1807db13b73f9990ac3704eadba49b65ee5c

msm8226.mk
@@ -127,9 +127,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    keystore.msm8226
+    android.hardware.keymaster@3.0-service
 
 # LiveDisplay
 PRODUCT_PACKAGES += \

Dheeraj3031A/android_device_motorola_msm8226-common

msm8226.mk
@@ -267,8 +267,7 @@ PRODUCT_COPY_FILES += \
 
 # Keystore
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    keystore.msm8226
+    android.hardware.keymaster@3.0-impl 
 
 # Lights
 PRODUCT_PACKAGES += \